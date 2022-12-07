e1f9b522132d6b1618d087fb168e84b308912b01
fazaimron27 <cincaumyself@gmail.com>
2022-12-07 19:36:11 +0700

    menambahkan provider clinic detail

--- lib/providers/clinic_detail_provider.dart
+++ lib/providers/clinic_detail_provider.dart
@@ -1,43 +1,64 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:pet_care_mobile_apps/data/api/api_service.dart';
 import 'package:pet_care_mobile_apps/data/models/clinic_detail_model.dart';
+import 'package:pet_care_mobile_apps/data/models/error_response.dart';
 import 'package:pet_care_mobile_apps/utils/result_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ClinicDetailProvider extends ChangeNotifier {
   final ApiService apiService;
-  final ClinicDetailModel clinicDetailModel;
+  int id;
 
-  ClinicDetailProvider({
-    required this.apiService,
-    required this.clinicDetailModel,
-  });
+  ClinicDetailProvider({required this.apiService, required this.id}) {
+    fetchClinicDetail();
+  }
 
   late ClinicDetailModel _clinicDetailModel;
-  late ResultState _state;
+  late ErrorResponse _errorResponse;
+  ResultState? _state;
   String _message = '';
 
   ClinicDetailModel get detail => _clinicDetailModel;
-  ResultState get state => _state;
+  ErrorResponse get resultError => _errorResponse;
+  ResultState? get state => _state;
   String get message => _message;
 
-  Future fetchAllClinicDetail(String accessToken, int clinicId) async {
+  Future fetchClinicDetail() async {
+    final dataUserAuth = await getDataUserAuth();
+    final accessToken = dataUserAuth['accessToken'];
+    final origin = dataUserAuth['address'];
+
     try {
       _state = ResultState.loading;
       notifyListeners();
-      final response = await apiService.getClinicDetail(accessToken, clinicId);
-      if (response.error) {
-        _state = ResultState.error;
-        notifyListeners();
-        return _message = response.message;
-      } else {
+      final response =
+          await apiService.getClinicDetail(accessToken, origin, id);
+      if (!response.error) {
         _state = ResultState.hasData;
         notifyListeners();
         return _clinicDetailModel = response;
+      } else {
+        if (response is ErrorResponse) {
+          _state = ResultState.error;
+          notifyListeners();
+          return _errorResponse = response;
+        } else {
+          _state = ResultState.error;
+          notifyListeners();
+          return _message = response.message;
+        }
       }
     } catch (e) {
       _state = ResultState.error;
       notifyListeners();
       return _message = 'Error: $e';
     }
+  }
+
+  Future<dynamic> getDataUserAuth() async {
+    final prefs = await SharedPreferences.getInstance();
+    final dataUserAuth = prefs.getString('DATA_USER_AUTH') ?? [];
+    return json.decode(dataUserAuth as String);
   }
 }
